7016d8fca08f23c8d898aabc9f22ed5763a0a2a4
Matheus Siqueira <m03.siqueira@gmail.com>
2025-03-29 13:22:01 -0300

    update js

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,14 @@
+document.querySelectorAll('.navbar__link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
+        const targetId = this.getAttribute('href').substring(1); // Remove o #
+        const targetSection = document.getElementById(targetId);
 
-
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50, // Ajuste conforme necessário
+                behavior: 'smooth'
+            });
+        }
+    });
+});
